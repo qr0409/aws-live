0373582ca85fbac0ee6bba326a0cf3404b772315
qr0409 <157588096+qr0409@users.noreply.github.com>
2025-12-10 16:30:25 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "employee.cpu08qeagoch.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "yongqiaorou-employee "
+customregion = "us-east-1"
+
